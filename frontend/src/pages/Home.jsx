2aec3d8d87222e6799a4f8affacd3e03c1e56375
Lucas TEAR <lucas.tear+1@efrei.net>
2024-11-03 02:45:25 +0100

    Add Commercial Contact Section
    - Added Commercial Contact Section to the page
    - Included images for the section
    - Implemented mailto button for direct contact

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,6 +5,8 @@ import Carousel from '../components/Carousel';
 import CollabIcon from '../assets/images/HomePage/Banner/CollabIcon.png';
 import ClientsIcon from '../assets/images/HomePage/Banner/ClientsIcon.png';
 import PartenairesIcon from '../assets/images/HomePage/Banner/PartenairesIcon.png';
+import DotCheckdIcon from '../assets/images/HomePage/DotCheckedIcon.png';
+import HomePageContent from '../assets/images/HomePage/HomePageContent.png';
 
 function Home() {
     return (
@@ -32,6 +34,23 @@ function Home() {
                 <div className="banner-box">
                     <img src={PartenairesIcon} alt="Partenaires" className="banner-icon" />
                     <span>+600 partenaires partout en France</span>
+                </div>
+            </div>
+            <div className="content-section">
+                <div className="left-box">
+                    <h3>Les atouts d'une maintenance informatique externalisée</h3>
+                    <ul>
+                        <li><img src={DotCheckdIcon} alt="Dot Check" className="dot-icon" /> Un support disponible à distance pour plus d'efficacité</li>
+                        <li><img src={DotCheckdIcon} alt="Dot Check" className="dot-icon" /> Un interlocuteur proche de vous et de vos employés</li>
+                        <li><img src={DotCheckdIcon} alt="Dot Check" className="dot-icon" /> Une technologie de pointe, au service de la simplicité</li>
+                        <li><img src={DotCheckdIcon} alt="Dot Check" className="dot-icon" /> Services clés en main: adaptables et évolutifs</li>
+                    </ul>
+                    <a href="mailto:contact@it8-solutions.com?subject=Objet%20Demande%20de%20Renseignement">
+                        <button className="contact-button">Contacter un Commercial</button>
+                    </a>
+                </div>
+                <div className="right-box">
+                    <img src={HomePageContent} alt="Contenu de la page" className="content-image" />
                 </div>
             </div>
         </div>
