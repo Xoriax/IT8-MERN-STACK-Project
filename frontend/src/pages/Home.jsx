1d5e92b6805e28cb046e520c754ac4182407069c
Lucas TEAR <lucas.tear+1@efrei.net>
2024-11-03 01:30:59 +0100

    Update banners and carousel styles
    - Added a banner displaying statistics
    - Removed a line from Carousel.css
    - Added images for the stats banner
    - Made minor adjustments to the hero banner

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import '../styles/Home.css';
 import Carousel from '../components/Carousel';
 
+import CollabIcon from '../assets/images/HomePage/Banner/CollabIcon.png';
+import ClientsIcon from '../assets/images/HomePage/Banner/ClientsIcon.png';
+import PartenairesIcon from '../assets/images/HomePage/Banner/PartenairesIcon.png';
+
 function Home() {
     return (
         <div className="home">
@@ -16,6 +20,20 @@ function Home() {
             <h2 className="page-title">Nos Prestations</h2>
             <p className="description">Nous avons une solution pour chacune de vos problématiques, que ce soit en délégation de personnel ou en régie externalisée.</p>
             <Carousel />
+            <div className="banner">
+                <div className="banner-box">
+                    <img src={CollabIcon} alt="Collaborateurs" className="banner-icon" />
+                    <span>19 Collaborateurs</span>
+                </div>
+                <div className="banner-box">
+                    <img src={ClientsIcon} alt="Clients" className="banner-icon" />
+                    <span>+50 clients</span>
+                </div>
+                <div className="banner-box">
+                    <img src={PartenairesIcon} alt="Partenaires" className="banner-icon" />
+                    <span>+600 partenaires partout en France</span>
+                </div>
+            </div>
         </div>
     );
 }
