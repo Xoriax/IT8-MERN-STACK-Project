af865f578ce4b7510a1870fa4876cd96d0b95636
Lucas TEAR <lucas.tear+1@efrei.net>
2024-11-05 15:37:44 +0100

    Update Card Components

--- frontend/src/components/CardDavid.jsx
+++ frontend/src/components/CardDavid.jsx
@@ -5,36 +5,33 @@ import LinkedinIcon from '../assets/images/Card/LinkedinIcon.png';
 
 function CardDavid() {
     return (
-        <div className="container-card">
-            <div className="card">
-                <div className="card-inner">
-
-                    <div className="front">
-                        <h2 className="front-card-h2">DAVID DO</h2>
-                        <p className="front-card-p">PRÉSIDENT <br /> DIRECTEUR COMMERCIAL <br /> IT8 SOLUTIONS, FRANCE</p>
-                        <button className="front-card-btn">MORE INFORMATION</button>
+        <div className="container-card-david">
+            <div className="card-david">
+                <div className="card-inner-david">
+                    <div className="front-david">
+                        <h2 className="front-card-h2-david">DAVID DO</h2>
+                        <p className="front-card-p-david">PRÉSIDENT <br /> DIRECTEUR COMMERCIAL <br /> IT8 SOLUTIONS, FRANCE</p>
+                        <button className="front-card-btn-david">MORE INFORMATION</button>
                     </div>
 
-                    <div className="back">
-
-                        <img className="back-card-img" src={WavingHand} alt="Waving hand" />
-                        <h1 className="back-card-h1">
-                            DAVID <span className="back-card-span">DO</span>
+                    <div className="back-david">
+                        <img className="back-card-img-david" src={WavingHand} alt="Waving hand" />
+                        <h1 className="back-card-h1-david">
+                            DAVID <span className="back-card-span-david">DO</span>
                         </h1>
-                        <p className="back-card-p">
-                            Texte présentation de la personne pour expliquer qui il est au sein d' <span className="back-card-span">IT8 SOLUTIONS</span>
+                        <p className="back-card-p-david">
+                            Texte présentation de la personne pour expliquer qui il est au sein d' <span className="back-card-span-david">IT8 SOLUTIONS</span>
                         </p>
 
-                        <div className="row">
+                        <div className="row-david">
                             <button>Follow</button>
                         </div>
 
-                        <div className="linkedin-row">
-                            <a href="https://www.linkedin.com/in/david-do-a1590046/" target='_blank' rel="noopener noreferrer" className="back-card-link">
+                        <div className="linkedin-row-david">
+                            <a href="https://www.linkedin.com/in/david-do-a1590046/" target='_blank' rel="noopener noreferrer" className="back-card-link-david">
                                 <img src={LinkedinIcon} alt="LinkedIn" />
                             </a>
                         </div>
-
                     </div>
                 </div>
             </div>
--- frontend/src/components/CardMichel.jsx
+++ frontend/src/components/CardMichel.jsx
@@ -1,40 +1,37 @@
 import React from 'react';
 import '../styles/Card/CardMichel.css';
 import WavingHand from '../assets/images/Card/waving-hand.png';
-import LinkedinIcon from '../assets/images/Card/LinkedinIcon.png'
+import LinkedinIcon from '../assets/images/Card/LinkedinIcon.png';
 
 function CardMichel() {
     return (
-        <div className="container-card">
-            <div className="card">
-                <div className="card-inner">
-
-                    <div className="front">
-                        <h2 className="front-card-h2">MICHEL KHAMXAY</h2>
-                        <p className="front-card-p">PRÉSIDENT, <br /> DIRECTEUR TECHNIQUE, <br /> IT8 SOLUTIONS, FRANCE</p>
-                        <button className="front-card-btn">MORE INFORMATION</button>
+        <div className="container-card-michel">
+            <div className="card-michel">
+                <div className="card-inner-michel">
+                    <div className="front-michel">
+                        <h2 className="front-card-h2-michel">MICHEL KHAMXAY</h2>
+                        <p className="front-card-p-michel">PRÉSIDENT, <br /> DIRECTEUR TECHNIQUE, <br /> IT8 SOLUTIONS, FRANCE</p>
+                        <button className="front-card-btn-michel">MORE INFORMATION</button>
                     </div>
 
-                    <div className="back">
-
-                        <img className="back-card-img" src={WavingHand} alt="Waving hand" />
-                        <h1 className="back-card-h1">
-                            MICHEL <span className="back-card-span">KHAMXAY</span>
+                    <div className="back-michel">
+                        <img className="back-card-img-michel" src={WavingHand} alt="Waving hand" />
+                        <h1 className="back-card-h1-michel">
+                            MICHEL <span className="back-card-span-michel">KHAMXAY</span>
                         </h1>
-                        <p className="back-card-p">
-                            Texte présentation de la personne pour expliquer qui il est au sein d' <span className="back-card-span">IT8 SOLUTIONS</span>
+                        <p className="back-card-p-michel">
+                            Texte présentation de la personne pour expliquer qui il est au sein d' <span className="back-card-span-michel">IT8 SOLUTIONS</span>
                         </p>
 
-                        <div className="row">
+                        <div className="row-michel">
                             <button>Follow</button>
                         </div>
 
-                        <div className="linkedin-row">
-                            <a href="/" target='_blank' rel="noopener noreferrer" className="back-card-link">
+                        <div className="linkedin-row-michel">
+                            <a href="/" target='_blank' rel="noopener noreferrer" className="back-card-link-michel">
                                 <img src={LinkedinIcon} alt="LinkedIn" />
                             </a>
                         </div>
-
                     </div>
                 </div>
             </div>
